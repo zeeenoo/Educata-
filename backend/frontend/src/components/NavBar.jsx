6eed12ad87ce087dfdf4ccf288bb6b7f4db8df56
Zouaoui Mohamed Cherif <cheriffzouaoui@gmail.com>
2023-01-28 01:59:55 +0100

    login popup

--- backend/frontend/src/components/NavBar.jsx
+++ backend/frontend/src/components/NavBar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import {ImMenu} from "react-icons/im"
 import {GrClose} from "react-icons/gr"
+import Signin from './Signin'
+
 
 function NavBar(props) {
   const [show, setShow] = React.useState(false)
@@ -12,6 +14,11 @@ function NavBar(props) {
                 <li className='p-2 mr-8'><a href='#' className=''>{item}</a></li>
             ))}  
         </ul>
+
+        <Signin/>
+        {/* <button className='border-2 rounded-2xl py-1 px-4 font-bold border-[#242145] hover:bg-[#2c705e] hover:text-white transition-colors duration-500 mr-4'>Login</button> */}
+        <button className='border-2 rounded-2xl py-1 px-4 font-bold border-[#242145] hover:bg-[#242145] hover:text-white transition-colors duration-500 mr-4'>sign-up</button>
+
         <ImMenu className={`${show? 'hidden': 'block'} sm:hidden mr-2 cursor-pointer`} size={30} onClick={()=> setShow(!show)}/>
         <GrClose className={`${show? 'block': 'hidden'} sm:hidden mr-2 cursor-pointer`} size={30} onClick={()=> setShow(!show)}/>
             
